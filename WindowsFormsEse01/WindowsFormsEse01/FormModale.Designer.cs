14c6393a692c7d1c9698dee95c48149a70e25d6c
Gabriele <marellogabriele@gmail.com>
2021-09-25 10:12:20 +0200

    Esercizio form 1
    Visualizzazione dinamica di ulteriori form partendo dalla main

---
+++ WindowsFormsEse01/WindowsFormsEse01/FormModale.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsEse01
+{
+    partial class FormModale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnnulla = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblEta = new System.Windows.Forms.Label();
+            this.txtEta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAnnulla
+            // 
+            this.btnAnnulla.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnAnnulla.Location = new System.Drawing.Point(186, 104);
+            this.btnAnnulla.Name = "btnAnnulla";
+            this.btnAnnulla.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnulla.TabIndex = 0;
+            this.btnAnnulla.Text = "Annulla";
+            this.btnAnnulla.UseVisualStyleBackColor = true;
+            this.btnAnnulla.Click += new System.EventHandler(this.btnAnnulla_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(41, 104);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(41, 31);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Nome";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(120, 31);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblEta
+            // 
+            this.lblEta.AutoSize = true;
+            this.lblEta.Location = new System.Drawing.Point(44, 71);
+            this.lblEta.Name = "lblEta";
+            this.lblEta.Size = new System.Drawing.Size(23, 13);
+            this.lblEta.TabIndex = 4;
+            this.lblEta.Text = "Età";
+            // 
+            // txtEta
+            // 
+            this.txtEta.Location = new System.Drawing.Point(120, 71);
+            this.txtEta.Name = "txtEta";
+            this.txtEta.Size = new System.Drawing.Size(100, 20);
+            this.txtEta.TabIndex = 5;
+            // 
+            // FormModale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 178);
+            this.Controls.Add(this.txtEta);
+            this.Controls.Add(this.lblEta);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnAnnulla);
+            this.Name = "FormModale";
+            this.Text = "FormModale";
+            this.Load += new System.EventHandler(this.FormModale_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnnulla;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblEta;
+        private System.Windows.Forms.TextBox txtEta;
+    }
+}
---
+++ WindowsFormsEse01/WindowsFormsEse01/FormModale.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsEse01
+{
+    public partial class FormModale : Form
+    {
+
+        public string nome = "";
+        public string eta = "";
+        public FormModale()
+        {
+            InitializeComponent();
+        }
+
+        private void FormModale_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            nome = txtName.Text;
+            eta = txtEta.Text;
+        }
+
+        private void btnAnnulla_Click(object sender, EventArgs e)
+        {
+            nome = "";
+            eta = "";
+        }
+    }
+}
